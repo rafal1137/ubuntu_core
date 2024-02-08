3d4ea5c28fc947455a3e7c86d6651bda2da67e07
rafal1137 <rafal1137@yahoo.pl>
2024-02-08 12:21:37 +0100

    qemu-bin: added new entry with network support

--- qemu-bin/qemu-system-x86_64.sh
+++ qemu-bin/qemu-system-x86_64.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 set -e
 
-sudo qemu-system-x86_64  -enable-kvm  -smp 6  -m 2048  -machine q35  -cpu host  -global ICH9-LPC.disable_s3=1  -net nic,model=virtio  -net user,hostfwd=tcp::8022-:22,hostfwd=tcp::8090-:80   -drive file=/usr/share/OVMF/OVMF_CODE.secboot.fd,if=pflash,format=raw,unit=0,readonly=on  -drive file=/usr/share/OVMF/OVMF_VARS.ms.fd,if=pflash,format=raw,unit=1  -chardev socket,id=chrtpm,path="/var/snap/test-snapd-swtpm/current/swtpm-sock"  -tpmdev emulator,id=tpm0,chardev=chrtpm  -device tpm-tis,tpmdev=tpm0  -drive "file=../ubuntu-core-22-amd64/pc.img",if=none,format=raw,id=disk1  -device virtio-blk-pci,drive=disk1,bootindex=1  -serial mon:stdio -vga qxl
+sudo kvm \
+ -smp 5 \
+ -m 2048 \
+ -machine q35 \
+ -cpu host \
+ -global ICH9-LPC.disable_s3=1 \
+ -netdev user,id=mynet0,hostfwd=tcp::8022-:22,hostfwd=tcp::8090-:80 -device virtio-net-pci,netdev=mynet0 \
+ -drive file=/usr/share/OVMF/OVMF_CODE.secboot.fd,if=pflash,format=raw,unit=0,readonly=on \
+ -drive file=/usr/share/OVMF/OVMF_VARS.ms.fd,if=pflash,format=raw,unit=1 \
+ -drive "file=../ubuntu-core-22-amd64/pc.img",if=none,format=raw,id=disk1 \
+ -device virtio-blk-pci,drive=disk1,bootindex=1 \
+ -serial mon:stdio -vga qxl
+
+#sudo qemu-system-x86_64  -enable-kvm  -smp 6  -m 2048  -machine q35  -cpu host  -global ICH9-LPC.disable_s3=1  -net nic,model=virtio  -net user,hostfwd=tcp::8022-:22,hostfwd=tcp::8090-:80   -drive file=/usr/share/OVMF/OVMF_CODE.secboot.fd,if=pflash,format=raw,unit=0,readonly=on  -drive file=/usr/share/OVMF/OVMF_VARS.ms.fd,if=pflash,format=raw,unit=1  -chardev socket,id=chrtpm,path="/var/snap/test-snapd-swtpm/current/swtpm-sock"  -tpmdev emulator,id=tpm0,chardev=chrtpm  -device tpm-tis,tpmdev=tpm0  -drive "file=../ubuntu-core-22-amd64/pc.img",if=none,format=raw,id=disk1  -device virtio-blk-pci,drive=disk1,bootindex=1  -serial mon:stdio -vga qxl
